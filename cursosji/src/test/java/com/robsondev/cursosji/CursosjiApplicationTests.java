2b6576cade0ca79deaaef18f018f58eccbf10bc3
Robson Pereira <robsonpereirape@gmail.com>
2020-10-26 10:09:45 -0300

    primeiro commit

---
+++ cursosji/src/test/java/com/robsondev/cursosji/CursosjiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robsondev.cursosji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursosjiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
